e811973d12953b94e6c3cfe8c1d4939899820001
kireeti ganisetti <kireeti17@gmail.com>
2021-07-22 10:15:32 +0530

    Implemented search feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,18 @@ const Main: React.FC<Props> = ({students}) => {
     setSearchText(event.target.value);
    }
 
+   // this to filter when user stopped typing
+   useDebouncedEffect(() => filterText(searchText), [searchText], 500);
+
+  const isValuePresent = (s:string, val: string) => {
+    return s.toLowerCase().includes(val);
+  }
+
+  const filterText = (val:string) => {
+    const filteredStudents = students.filter(s => isValuePresent(s.first_name, val) || isValuePresent(s.last_name, val));
+    setStudentList(filteredStudents);
+  }
+
   return (
     <Box direction="column" pad="medium" height="100%" overflow="auto">
       <TextInput placeholder="type here" value={searchText} onChange={onChangeHandler} />
@@ -37,5 +49,13 @@ export const getServerSideProps = async (context:any) => {
   }
 }
 
+// normally i add this in hooks folder where we keep custom hooks
+export const useDebouncedEffect = (effect:any, deps:any, delay:number) => {
+    useEffect(() => {
+        const handler = setTimeout(() => effect(), delay);
+
+        return () => clearTimeout(handler);
+    }, [...deps || [], delay]);
+}
 
 export default Main;
