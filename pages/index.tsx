9984b60704d5124c13ff1192d863c10c6c03f19c
kireeti ganisetti <kireeti17@gmail.com>
2021-07-22 10:07:41 +0530

    populated student list i

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,27 +1,41 @@
 import { Box, TextInput } from "grommet";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import UserCard from "../components/user.card";
 import { fetchStudents, Student } from "../services/students";
 
-type Props = {};
+type Props = {
+  students: Student[]
+};
 
-const Main: React.FC<Props> = ({}) => {
-  const onChangeHandler = (event: any) => {
-    // TODO
-  };
+const Main: React.FC<Props> = ({students}) => {
+  const [searchText, setSearchText] = useState('');
+  const [studentList, setStudentList] = useState(students);
+
+   const onChangeHandler = (event: any) => {
+    setSearchText(event.target.value);
+   }
 
   return (
     <Box direction="column" pad="medium" height="100%" overflow="auto">
-      <TextInput placeholder="type here" value="" onChange={onChangeHandler} />
+      <TextInput placeholder="type here" value={searchText} onChange={onChangeHandler} />
       <Box direction="row" wrap={true}>
-        {/* {students.map((s) => (
-          <Box margin="10px">
+        {studentList.map((s) => (
+          <Box margin="10px" key={s.id}>
             <UserCard user={s} />
           </Box>
-        ))} */}
+        ))}
       </Box>
     </Box>
   );
 };
+
+export const getServerSideProps = async (context:any) => {
+  const students = await fetchStudents();
+
+  return {
+    props: { students }, // will be passed to the page component as props
+  }
+}
+
 
 export default Main;
