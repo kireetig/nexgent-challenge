c787c3fc1224f420ad82cb1a69c300fb3a9e9974
kireeti ganisetti <kireeti17@gmail.com>
2021-07-24 13:41:33 +0530

    implemented pagination to increase performance

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,13 @@
-import { Box, Grid, ResponsiveContext, Spinner, TextInput } from "grommet";
+import {
+  Box,
+  Grid,
+  ResponsiveContext,
+  Spinner,
+  TextInput,
+  Text,
+  Pagination,
+  Heading,
+} from "grommet";
 import React, { useContext, useEffect, useState } from "react";
 import UserCard from "../components/user.card";
 import { fetchStudents, Student } from "../services/students";
@@ -11,11 +20,20 @@ type Props = {
 
 const Main: React.FC<Props> = ({ students }) => {
   const router = useRouter();
+  const pageSize = 12; // number of items per page to be shown
   const [searchText, setSearchText] = useState<string>(
     (router.query.search as string) || ""
   );
-  const [studentList, setStudentList] = useState<Student[]>([]);
-  const [isloading, setIsLoading] = useState<boolean>(true);
+  const [studentList, setStudentList] = useState<Student[]>(() =>
+    router.query.search ? [] : students
+  );
+  const [isloading, setIsLoading] = useState<boolean>(
+    () => !!router.query.search
+  );
+  const [currentData, setCurrentData] = useState(() =>
+    router.query.search ? [] : students.slice(0, pageSize)
+  );
+  const [indices, setIndices] = useState([0, pageSize]);
   const size = useContext(ResponsiveContext);
 
   const onChangeHandler = (event: any) => {
@@ -23,8 +41,20 @@ const Main: React.FC<Props> = ({ students }) => {
     setSearchText(event.target.value);
   };
 
+  const handleChange = ({
+    startIndex,
+    endIndex,
+  }: {
+    startIndex: number;
+    endIndex: number;
+  }) => {
+    const nextData = studentList.slice(startIndex, endIndex);
+    setCurrentData(nextData);
+    setIndices([startIndex, Math.min(endIndex, studentList.length)]);
+  };
+
   // this to filter when user stopped typing
-  useDebouncedEffect(() => filterText(searchText), [searchText], 250);
+  useDebouncedEffect(() => filterText(searchText), [searchText], 300);
 
   const isValuePresent = (s: string, val: string) => {
     return s.toLowerCase().includes(val);
@@ -32,17 +62,21 @@ const Main: React.FC<Props> = ({ students }) => {
 
   const filterText = (val: string) => {
     // to persist search text
-    router.push({
-      pathname: "/",
-      query: { search: val },
-    });
-    router.push(router);
+    router.push(
+      {
+        query: { search: val },
+      },
+      undefined,
+      { shallow: true }
+    );
     const filteredStudents = students.filter(
       (s) =>
         isValuePresent(s.first_name, val) || isValuePresent(s.last_name, val)
     );
     setIsLoading(false);
     setStudentList(filteredStudents);
+    setIndices([0, pageSize]);
+    setCurrentData(filteredStudents.slice(0, pageSize));
   };
 
   return (
@@ -64,7 +98,7 @@ const Main: React.FC<Props> = ({ students }) => {
           justify={"center"}
         >
           {!isloading &&
-            studentList.map((s) => (
+            currentData.map((s) => (
               <Link href={`/student/${s.id}`} key={s.id}>
                 <Box margin="10px" alignSelf="center">
                   <UserCard user={s} />
@@ -72,6 +106,24 @@ const Main: React.FC<Props> = ({ students }) => {
               </Link>
             ))}
         </Grid>
+        {!isloading && currentData.length === 0 && (
+          <Heading>Ops! No results found</Heading>
+        )}
+        {currentData.length !== 0 && (
+          <Box align="center" margin="30px 0" direction="row" justify="between">
+            <Text>
+              Showing {indices[0] + 1} -{" "}
+              {currentData.length < indices[1]
+                ? currentData.length
+                : indices[1]}{" "}
+              of {studentList.length}
+            </Text>
+            <Pagination
+              numberItems={studentList.length}
+              onChange={handleChange}
+            />
+          </Box>
+        )}
       </Box>
     </Box>
   );
