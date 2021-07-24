07c01ebe838620d5b71082014c3883d1484e2a9d
kireeti ganisetti <kireeti17@gmail.com>
2021-07-24 15:20:29 +0530

    added env variables for deployment

--- services/students.ts
+++ services/students.ts
@@ -8,14 +8,16 @@ export type Student = {
   company: string;
 };
 
+const HOST = process.env.HOST;
+
 export const fetchStudents = async (): Promise<Student[]> => {
-  const response = await fetch("http://localhost:3000/api/students");
+  const response = await fetch(`${HOST}/api/students`);
   const data = await response.json();
   return data;
 };
 
 export const fetchStudent = async (id: string): Promise<Student> => {
-  const response = await fetch(`http://localhost:3000/api/students/${id}`);
+  const response = await fetch(`${HOST}/api/students/${id}`);
   const data = await response.json();
   return data;
 };
